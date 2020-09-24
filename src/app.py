8b657ba15bae377f43b9e6cedfad7ecf830425dd
Italo Carvalho <italo.998.carvalho@gmail.com>
2020-09-24 18:42:03 -0300

    Add docker database

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/db_rows_versioning'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@localhost:15432/db_rows_versioning'
 db = SQLAlchemy(app)
 
 from users.view import user_api
